cdcf26e0d5297553565dedf5c9847ce2bc908560
Jesús Di Bari <jesus.dibari@paack.co>
2020-11-12 00:17:25 +0100

    Indent correctly

--- matematica.rb
+++ matematica.rb
@@ -1,27 +1,27 @@
 class Complejos
-    def initialize(a, b)
-        @a = a
-        @b = b    
-    end
-    def suma(c_b)
-        suma_r = @a + c_b.get_a.to_i
-        suma_i = @b + c_b.get_b.to_i
-        puts "(#{suma_r}, #{suma_i}i)"
-    end
-    def resta(c_b)
-        resta_r = @a - c_b.get_a.to_i
-        resta_i = @b - c_b.get_b.to_i
-        puts "(#{resta_r}, #{resta_i}i)"
-    end
-    def producto(r)
-        puts "(#{@a*r}, #{@b*r}i)"
-    end
-    def get_a
-         @a
-    end
-    def get_b
-         @b
-    end
+  def initialize(a, b)
+    @a = a
+    @b = b    
+  end
+  def suma(c_b)
+    suma_r = @a + c_b.get_a.to_i
+    suma_i = @b + c_b.get_b.to_i
+    puts "(#{suma_r}, #{suma_i}i)"
+  end
+  def resta(c_b)
+    resta_r = @a - c_b.get_a.to_i
+    resta_i = @b - c_b.get_b.to_i
+    puts "(#{resta_r}, #{resta_i}i)"
+  end
+  def producto(r)
+    puts "(#{@a*r}, #{@b*r}i)"
+  end
+  def get_a
+    @a
+  end
+  def get_b
+    @b
+  end
 end
 
 a_complejo = Complejos.new(5,8)
