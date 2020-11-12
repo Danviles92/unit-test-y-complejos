cc2af4490fbd3338b71821da5fdca5221b4409ca
Jesús Di Bari <jesus.dibari@paack.co>
2020-11-12 01:02:40 +0100

    Add complejizar method

--- matematica.rb
+++ matematica.rb
@@ -5,7 +5,15 @@ class Complejos
     @a = a
     @b = b    
   end
-  
+
+  def complejizar
+    "(#{a} + #{b}i)"
+  end
+
+  def self.suma(complejo_a, complejo_b)
+    
+  end
+
   def suma(c_b)
     suma_r = @a + c_b.get_a.to_i
     suma_i = @b + c_b.get_b.to_i
@@ -25,14 +33,17 @@ end
 
 a_complejo = Complejos.new(5,8)
 b_complejo = Complejos.new(2,3)
-r = 5
 
-suma = a_complejo.suma(b_complejo)
-resta = a_complejo.resta(b_complejo)
-producto_escalar = a_complejo.producto(r)
+puts a_complejo.complejizar
 
-puts "Suma de #{a_complejo} con #{b_complejo} es = #{suma}"
-puts "Resta de #{a_complejo} con #{b_complejo} es = #{resta}"
-puts "Producto escalar de #{a_complejo} con #{r} es = #{producto_escalar}"
+# r = 5
+
+# suma = a_complejo.suma(b_complejo)
+# resta = a_complejo.resta(b_complejo)
+# producto_escalar = a_complejo.producto(r)
+
+# puts "Suma de #{a_complejo} con #{b_complejo} es = #{suma}"
+# puts "Resta de #{a_complejo} con #{b_complejo} es = #{resta}"
+# puts "Producto escalar de #{a_complejo} con #{r} es = #{producto_escalar}"
 
 
