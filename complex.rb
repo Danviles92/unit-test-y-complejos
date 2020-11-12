734429fe81688d57a3220201d11d149a5c4b8d6e
Jesús Di Bari <jesus.dibari@paack.co>
2020-11-12 01:52:51 +0100

    Add subtract and multiply

--- complex.rb
+++ complex.rb
@@ -15,11 +15,19 @@ class ComplexN
   def self.sum(complex_a, complex_b)
     ComplexN.new(complex_a.a + complex_b.a, complex_a.b + complex_b.b)
   end
+
+  def self.subtract(complex_a, complex_b)
+    ComplexN.new(complex_a.a - complex_b.a, complex_a.b - complex_b.b)
+  end
+
+  def self.multiply(scalar, complex)
+    ComplexN.new(scalar * complex.a, scalar * complex.b)
+  end
 end
 
-complex_a = ComplexN.new(5, -8)
-complex_b = ComplexN.new(2, 3)
-
-puts complex_a.complexize
+complex_a = ComplexN.new(2, 2)
+complex_b = ComplexN.new(2, -3)
 
 puts ComplexN.sum(complex_a, complex_b).complexize
+puts ComplexN.subtract(complex_a, complex_b).complexize
+puts ComplexN.multiply(5, complex_b).complexize
