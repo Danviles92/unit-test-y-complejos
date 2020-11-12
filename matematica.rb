6f746e840ca268123319814cc414787bc1c333c9
Elvis Nogueiras <alu0101281308@ull.edu.es>
2020-11-12 00:28:14 +0000

    Suma ahora es un metodo de clase

--- matematica.rb
+++ matematica.rb
@@ -1,37 +1,48 @@
 class Complejos
-  attr_reader :a, :b
-
-  def initialize(a, b)
-    @a = a
-    @b = b    
+    attr_reader :a, :b
+  
+    def initialize(a, b)
+      @a = a
+      @b = b    
+    end
+  
+    def complejizar
+      "(#{a} + #{b}i)"
+    end
+  
+    def self.suma(complejo_a, complejo_b)
+      "(#{complejo_a.a + complejo_b.a} + #{complejo_a.b + complejo_b.b}i)"
+    end
+  
+    def suma(c_b)
+      suma_r = @a + c_b.get_a.to_i
+      suma_i = @b + c_b.get_b.to_i
+      puts "(#{suma_r}, #{suma_i}i)"
+    end
+  
+    def resta(c_b)
+      resta_r = @a - c_b.get_a.to_i
+      resta_i = @b - c_b.get_b.to_i
+      puts "(#{resta_r}, #{resta_i}i)"
+    end
+  
+    def producto(r)
+      puts "(#{@a*r}, #{@b*r}i)"
+    end
   end
   
-  def self.suma(complejo_a, complejo_b)
-    suma_r = @a + complejo_a.a.to_i
-    suma_i = @b + ccomplejo_b.b.to_i
-    puts "(#{suma_r}, #{suma_i}i)"
-  end
-
-  def self.resta(complejo_a, complejo_b)
-    resta_r = @a - ccomplejo_a.a.to_i
-    resta_i = @b - ccomplejo_b.b.to_i
-    puts "(#{resta_r}, #{resta_i}i)"
-  end
-
-  def self.producto(complejo, producto)
-    puts "(#{complejo.a*r}, #{complejo.b*r}i)"
-  end
-end
-
-a_complejo = Complejos.new(5,8)
-b_complejo = Complejos.new(2,3)
-
-suma = Complejos.suma(a_complejo, b_complejo)
-resta = Complejos.resta(a_complejo, b_complejo)
-producto = Complejos.producto(a_complejo, r)
-
-# puts "Suma de #{a_complejo} con #{b_complejo} es = #{suma}"
-# puts "Resta de #{a_complejo} con #{b_complejo} es = #{resta}"
-# puts "Producto escalar de #{a_complejo} con #{r} es = #{producto_escalar}"
-
-
+  a_complejo = Complejos.new(5,8)
+  b_complejo = Complejos.new(2,3)
+  
+  puts a_complejo.complejizar
+  puts Complejos.suma(a_complejo, b_complejo)
+  
+  # r = 5
+  
+  # suma = a_complejo.suma(b_complejo)
+  # resta = a_complejo.resta(b_complejo)
+  # producto_escalar = a_complejo.producto(r)
+  
+  # puts "Suma de #{a_complejo} con #{b_complejo} es = #{suma}"
+  # puts "Resta de #{a_complejo} con #{b_complejo} es = #{resta}"
+  # puts "Producto escalar de #{a_complejo} con #{r} es = #{producto_escalar}"
