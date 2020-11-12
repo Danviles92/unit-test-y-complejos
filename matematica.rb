d5b8486143f6b4c8303542d1da4be260504b2cc3
Elvis Nogueiras <alu0101281308@ull.edu.es>
2020-11-12 00:11:50 +0000

    arreglando cosas en el codigo 1

--- matematica.rb
+++ matematica.rb
@@ -6,20 +6,20 @@ class Complejos
     @b = b    
   end
   
-  def suma(c_b)
+  def suma(complejo_a, complejo_b)
     suma_r = @a + c_b.get_a.to_i
     suma_i = @b + c_b.get_b.to_i
     puts "(#{suma_r}, #{suma_i}i)"
   end
 
-  def resta(c_b)
+  def resta(complejo_a, complejo_b)
     resta_r = @a - c_b.get_a.to_i
     resta_i = @b - c_b.get_b.to_i
     puts "(#{resta_r}, #{resta_i}i)"
   end
 
-  def producto(r)
-    puts "(#{@a*r}, #{@b*r}i)"
+  def producto(complejo, producto)
+    puts "(#{complejo.a*r}, #{complejo.b*r}i)"
   end
 end
 
@@ -27,9 +27,9 @@ a_complejo = Complejos.new(5,8)
 b_complejo = Complejos.new(2,3)
 r = 5
 
-suma = a_complejo.suma(b_complejo)
-resta = a_complejo.resta(b_complejo)
-producto_escalar = a_complejo.producto(r)
+suma = Complejos.suma(a_complejo, b_complejo)
+resta = Complejos.resta(a_complejo, b_complejo)
+producto = Complejos.producto(a_complejo, r)
 
 puts "Suma de #{a_complejo} con #{b_complejo} es = #{suma}"
 puts "Resta de #{a_complejo} con #{b_complejo} es = #{resta}"
